8e48a8ede0e0f276ceefbec0db369e3aff009277
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-12-18 19:01:09 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ import { Logger } from './logger';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// ROUTES
+import nnas from './services/nnas';
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: true
+}));
+
+app.use(nnas);
 
 app.listen(process.env.HTTP_PORT, () => {
   Logger.info(`The server was listening on http://localhost:${process.env.HTTP_PORT}`);
